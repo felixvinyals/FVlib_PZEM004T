0011877738e3f9a57267b1b0b40081022f45d0c7
Fèlix Vinyals <felix@felixvinyals.com>
2017-06-16 14:09:14 +0200

    Finished

--- FVlib_PZEM004T.cpp
+++ FVlib_PZEM004T.cpp
@@ -8,29 +8,52 @@ FVlib_PZEM004T::FVlib_PZEM004T(SoftwareSerial &_port) {
 }
 
 float FVlib_PZEM004T::readVoltage() {
+  float voltage = 0;
+
   //Set adress:
   setCommunicationAddress();
 
-  /*
-  //Read voltage:
-  (*softPort).write(0xB0);
-  (*softPort).write(0xC0);
-  (*softPort).write(0xA8);
-  (*softPort).write(0x01);
-  (*softPort).write(0x01);
-  (*softPort).write((byte) 0x00);
-  (*softPort).write(0x1E);
-  delay(500);
-  Serial.print("voltage:");
-  receive(true); */
-
-  Serial.print("voltage:");
   (*softPort).write(askVoltage, 7);
-  //delay(500);
-  receive(true);
+  receive(false);
 
+  voltage = (float) buffer[3] / 10;
+  voltage = voltage + buffer[2];
+  return(voltage);
+}
+
+float FVlib_PZEM004T::readCurrent() {
+  float current = 0;
+
+  setCommunicationAddress();
+
+  (*softPort).write(askCurrent, 7);
+  receive(false);
+
+  if (buffer[3] < 10) current = (float) buffer[3] / 10;
+  else current = (float) buffer[3] / 100;
+  current = current + buffer[2];
+  return(current);
 
 }
+
+unsigned int FVlib_PZEM004T::readPower() {
+  unsigned int power = 0;
+
+  setCommunicationAddress();
+
+  (*softPort).write(askPower, 7);
+  receive(false);
+
+  //Serial.println(buffer[1]);
+  //Serial.println(buffer[2]);
+
+  power = buffer[1];
+  power = power << 8;
+  power = power + buffer[2];
+  return(power);
+
+}
+
 
 /*boolean FVlib_PZEM004T::send(byte* message) {
   (*softPort).write(*message, 7);
@@ -45,7 +68,13 @@ boolean FVlib_PZEM004T::receive(boolean printBuffer) {
       if (printBuffer == true) Serial.println(buffer[index], HEX);
     }
     if((*softPort).available() > 0) return true;
-    else return false;
+    else {
+      if (checksumError() == 0) return false;
+      else {
+        return true;
+      }
+    }
+
 }
 
 boolean FVlib_PZEM004T::waitBufferToFill() {
@@ -57,9 +86,24 @@ boolean FVlib_PZEM004T::waitBufferToFill() {
   return false;
 }
 
+boolean FVlib_PZEM004T::checksumError() {
+// Return: false Ok, true Error
+  byte checksum_Sum = 0;
+
+  for (byte index = 0; index < 6; index++) {
+    checksum_Sum = checksum_Sum + buffer[index];
+  }
+  if (checksum_Sum == buffer[6]) return false;
+  else {
+    Serial.print("CHKSUM Error: ");
+    Serial.print("Calculated: ");
+    Serial.println(checksum_Sum);
+    return true;
+  }
+}
+
 void FVlib_PZEM004T::setCommunicationAddress() {
-  Serial.println("Setting comunication address");
+  //Serial.println("Setting comunication address");
   (*softPort).write(setAddress, 7);
-  //delay(500);
-  receive(true);
+  receive(false);
 }
--- FVlib_PZEM004T.h
+++ FVlib_PZEM004T.h
@@ -9,6 +9,8 @@
       // Routines:
       FVlib_PZEM004T::FVlib_PZEM004T(SoftwareSerial &_port);
       float FVlib_PZEM004T::readVoltage();
+      float FVlib_PZEM004T::readCurrent();
+      unsigned int FVlib_PZEM004T::readPower();
 
 
     private:
@@ -16,14 +18,15 @@
       void FVlib_PZEM004T::setCommunicationAddress();
       boolean FVlib_PZEM004T::receive(boolean printBuffer);
       boolean FVlib_PZEM004T::waitBufferToFill();
+      boolean FVlib_PZEM004T::checksumError();
       //boolean FVlib_PZEM004T::send(byte* message);
       // Variables:
       SoftwareSerial *softPort;
       byte buffer[7];
       byte askVoltage[7] = {0xB0, 0xC0, 0xA8, 0x01, 0x01, (byte) 0x00, 0x1E};
+      byte askCurrent[7] = {0xB1, 0xC0, 0xA8, 0x01, 0x01, (byte) 0x00, 0x1B};
+      byte askPower[7] = {0xB2, 0xC0, 0xA8, 0x01, 0x01, (byte) 0x00, 0x1C};
       byte setAddress[7] = {0xB4, 0xC0, 0xA8, 0x01, 0x01, (byte) 0x00, 0x1E};
-
-
   };
 
 
